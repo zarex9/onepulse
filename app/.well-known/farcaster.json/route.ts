d4f1fabf4d2e74cb9797317f082c7fcd9107fc78
irkham <121574317+10122222@users.noreply.github.com>
2025-10-26 00:46:07 +0700

    feat(minikit): restructure GET response to include detailed miniapp and baseBuilder configurations

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,6 +1,36 @@
-import { withValidManifest } from "@coinbase/onchainkit/minikit";
 import { minikitConfig } from "../../../minikit.config";
 
 export async function GET() {
-  return Response.json(withValidManifest(minikitConfig));
+  const config = {
+    "accountAssociation": {
+      "header": minikitConfig.accountAssociation.header,
+      "payload": minikitConfig.accountAssociation.payload,
+      "signature": minikitConfig.accountAssociation.signature
+    },
+    "miniapp": {
+      "version": minikitConfig.miniapp.version,
+      "name": minikitConfig.miniapp.name,
+      "subtitle": minikitConfig.miniapp.subtitle,
+      "description": minikitConfig.miniapp.description,
+      "screenshotUrls": minikitConfig.miniapp.screenshotUrls,
+      "iconUrl": minikitConfig.miniapp.iconUrl,
+      "splashImageUrl": minikitConfig.miniapp.splashImageUrl,
+      "splashBackgroundColor": minikitConfig.miniapp.splashBackgroundColor,
+      "homeUrl": minikitConfig.miniapp.homeUrl,
+      "webhookUrl": minikitConfig.miniapp.webhookUrl,
+      "primaryCategory": minikitConfig.miniapp.primaryCategory,
+      "tags": minikitConfig.miniapp.tags,
+      "heroImageUrl": minikitConfig.miniapp.heroImageUrl,
+      "tagline": minikitConfig.miniapp.tagline,
+      "ogTitle": minikitConfig.miniapp.ogTitle,
+      "ogDescription": minikitConfig.miniapp.ogDescription,
+      "ogImageUrl": minikitConfig.miniapp.ogImageUrl,
+      "noindex": minikitConfig.miniapp.noindex
+    },
+    "baseBuilder": {
+      "ownerAddress": minikitConfig.baseBuilder.ownerAddress,
+      "allowedAddresses": minikitConfig.baseBuilder.allowedAddresses
+    }
+  }
+  return Response.json(config);
 }
