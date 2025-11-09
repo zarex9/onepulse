d72f5b4aa1c2d7dbf2de811e784e30a3cef23590
irkham <121574317+10122222@users.noreply.github.com>
2025-11-10 03:15:55 +0700

    refactor(svg): add title

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -37,6 +37,7 @@ export function ModeToggle() {
         width="24"
         xmlns="http://www.w3.org/2000/svg"
       >
+        <title>Theme toggle icon</title>
         <path d="M0 0h24v24H0z" fill="none" stroke="none" />
         <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
         <path d="M12 3l0 18" />
