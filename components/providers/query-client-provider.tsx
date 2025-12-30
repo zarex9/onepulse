7977a76752360ddf1d4791ec25de3fc4b257144a
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2025-12-31 03:16:19 +0700

    refactor(query-client): simplify QueryClientProvider and remove unused logic

--- components/providers/query-client-provider.tsx
+++ components/providers/query-client-provider.tsx
@@ -1,21 +1,15 @@
 "use client";
 
-import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
-import { useQueryClientProviderLogic } from "./use-query-client-provider-logic";
+import { QueryClientProvider as Provider } from "@tanstack/react-query";
+import type { ReactNode } from "react";
+import { getQueryClient } from "@/lib/client";
 
 export default function QueryClientProvider({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
-  const { queryClient, persister } = useQueryClientProviderLogic();
+  const queryClient = getQueryClient();
 
-  return (
-    <PersistQueryClientProvider
-      client={queryClient}
-      persistOptions={{ persister }}
-    >
-      {children}
-    </PersistQueryClientProvider>
-  );
+  return <Provider client={queryClient}>{children}</Provider>;
 }
--- lib/client.ts
+++ lib/client.ts
@@ -1,10 +1,24 @@
-import { isServer, QueryClient } from "@tanstack/react-query";
+import {
+  defaultShouldDehydrateQuery,
+  isServer,
+  QueryClient,
+} from "@tanstack/react-query";
+import superjson from "superjson";
 
 function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 60 * 1000,
+        staleTime: 30 * 1000,
+      },
+      dehydrate: {
+        serializeData: superjson.serialize,
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) ||
+          query.state.status === "pending",
+      },
+      hydrate: {
+        deserializeData: superjson.deserialize,
       },
     },
   });
