aed276e6a8aaa4bfea6333ec8f86369096bf8b79
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2026-01-04 17:26:06 +0700

    refactor(user-data): replace memoized component with standard function component and update address handling

--- components/user-info/user-data.tsx
+++ components/user-info/user-data.tsx
@@ -1,30 +1,27 @@
 "use client";
 
-import { memo } from "react";
-import { truncateAddress } from "@/lib/ens-utils";
+import { getSlicedAddress } from "@/lib/ens-utils";
 
-export const UserData = memo(
-  ({
-    displayName,
-    username,
-    address,
-  }: {
-    displayName: string;
-    username?: string;
-    address?: string;
-  }) => {
-    const showUsername = username && address === undefined;
-    return (
-      <div className="flex flex-col gap-1 text-sm leading-none">
-        <span className="font-medium">{displayName}</span>
-        {showUsername ? (
-          <span className="text-muted-foreground">@{username}</span>
-        ) : (
-          <span className="text-muted-foreground">
-            {truncateAddress(address)}
-          </span>
-        )}
-      </div>
-    );
-  }
-);
+export function UserData({
+  displayName,
+  username,
+  address,
+}: {
+  displayName: string;
+  username?: string;
+  address?: `0x${string}`;
+}) {
+  const showUsername = username && address === undefined;
+  return (
+    <div className="flex flex-col gap-1 text-sm leading-none">
+      <span className="font-medium">{displayName}</span>
+      {showUsername ? (
+        <span className="text-muted-foreground">@{username}</span>
+      ) : (
+        <span className="text-muted-foreground">
+          {address ? getSlicedAddress(address) : ""}
+        </span>
+      )}
+    </div>
+  );
+}
