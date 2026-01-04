c78b0de81b8ed15df9d0581c8937cbcbeb21eeed
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2026-01-04 17:25:14 +0700

    refactor(user-info): replace memoized component with a standard function component

--- components/user-info/user-info-skeleton.tsx
+++ components/user-info/user-info-skeleton.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { memo } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 
-export const UserInfoSkeleton = memo(() => (
-  <div className="flex flex-col gap-1">
-    <Skeleton className="h-3.5 w-20 rounded" />
-    <Skeleton className="h-3.5 w-16 rounded" />
-  </div>
-));
+export function UserInfoSkeleton() {
+  return (
+    <div className="flex flex-col gap-1">
+      <Skeleton className="h-3.5 w-20 rounded" />
+      <Skeleton className="h-3.5 w-16 rounded" />
+    </div>
+  );
+}
