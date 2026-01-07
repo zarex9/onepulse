0fe2d4046a61a9416317812e29d2a9fb225f7988
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2026-01-07 23:42:42 +0700

    refactor(page-state): remove mini app context logic for simplification

--- hooks/use-page-state.ts
+++ hooks/use-page-state.ts
@@ -1,10 +1,7 @@
 import { useConnection } from "wagmi";
-import { useMiniAppContext } from "@/components/providers/miniapp-provider";
 
 export function usePageState() {
   const { address, isConnected } = useConnection();
-  const miniAppContextData = useMiniAppContext();
-  const inMiniApp = miniAppContextData?.isInMiniApp ?? false;
 
-  return { inMiniApp, isConnected, address };
+  return { isConnected, address };
 }
